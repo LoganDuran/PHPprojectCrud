ffd6d1ba093690a0ee27228e14dac4697f6cc378
LoganDuran <106428943+LoganDuran@users.noreply.github.com>
2024-01-11 18:30:15 +0100

    UniversalHeader implamented

--- header.php
+++ header.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tankophile.net</title>
+    <title>tankopedia.net</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
-    <header>
-        <h2 class="logo">Tankophile.net</h2>
+    <!-- <header>
+        <h2 class="logo">tanko<span>pedia.net</span></h2>
         
         <div class="navmenu">
         <div class="searchbox">
@@ -18,7 +21,47 @@
 
                 <button type="submit">Go</button> <br>
             </form>
-        </div>
+        </div> -->
+
+    <header class="header">
+        <h2 class="logo">tanko<span>pedia.net</span></h2>
+         <nav class="nav">
+          <ul class="navlist">
+            <li><div class="searchbox">
+                <form class="search-input" action="search.php" method="post">
+                <input id="search" type="text" name="usersearch" placeholder="Search..">
+                <button class="searchbutton" type="submit">Go</button> <br>
+                </form>
+            </div></li>
+            <li class="underline"><a href="index.php">Home</a></li>
+            <li class="underline"><a href="#">About</a></li>
+            <li class="underline"><a href="#">Contact</a></li>
+            <li><a class="loginbutton">
+               <?php 
+                    if(isset($_SESSION["userid"])){
+                      
+                     ?> 
+                     <button class="loginbutton"><?php echo $_SESSION["useruid"]; ?> </button>
+                     <?php              
+                    }
+                    else
+                    {
+                        ?>
+                        <button class="loginbutton">Login </button>
+                        <?php              
+                    } ?> 
+             </a></li>
+            </ul>
+        </nav>    
+        <div class = "hamMenu"><div class = "hamButton"></div></div>
+    </header>
+
+
+
+
+
+
+<!-- 
         <nav class="nav">
             <a href="index.php">Home</a>
             <a href="#">About</a>
@@ -41,7 +84,7 @@
                 ?> 
             </a>
          </nav>
-        </div>
+        </div> -->
     </header>
     <body>
         
